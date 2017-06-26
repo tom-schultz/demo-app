6b2b6aae554a295f213f8ed5741c952070efe116
Tom Schultz <tomschul@amazon.com>
2017-06-26 23:26:56 +0000

    Pathing attempt #436

--- scripts/pre_build.sh
+++ scripts/pre_build.sh
@@ -5,7 +5,7 @@ set -e
 pwd
 ls
 chmod +x version.cfg
-. version.cfg && bumpverison --current-version $VERSION minor version.cfg
+. ../version.cfg && bumpverison --current-version $VERSION minor ../version.cfg
 pylint buggworks
 pylint test
 python -m unittest discover
