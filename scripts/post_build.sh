8115beef0f4a15e41c11581520273c65d8abd6ac
Tom Schultz <tomschul@amazon.com>
2017-06-27 21:49:22 +0000

    Updating ecr repo.

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -10,9 +10,9 @@ cd $DIR/..
 $(aws ecr get-login --region $AWS_DEFAULT_REGION)
 chmod +x version.cfg
 . version.cfg
-docker build -t app -t app:$VERSION . 
-docker tag app:latest 950174139509.dkr.ecr.us-west-2.amazonaws.com/app:latest
-docker tag app:$VERSION 950174139509.dkr.ecr.us-west-2.amazonaws.com/app:$VERSION
-docker push 950174139509.dkr.ecr.us-west-2.amazonaws.com/app:latest
-docker push 950174139509.dkr.ecr.us-west-2.amazonaws.com/app:$VERSION
+docker build -t octank-demo-app -t octank-demo-app:$VERSION . 
+docker tag octank-demo-app:latest 950174139509.dkr.ecr.us-west-2.amazonaws.com/octank-demo-app:latest
+docker tag octank-demo-app:$VERSION 950174139509.dkr.ecr.us-west-2.amazonaws.com/octank-demo-app:$VERSION
+docker push 950174139509.dkr.ecr.us-west-2.amazonaws.com/octank-demo-app:latest
+docker push 950174139509.dkr.ecr.us-west-2.amazonaws.com/octank-demo-app:$VERSION
 echo "{'version': '$VERSION'}" > build.json
